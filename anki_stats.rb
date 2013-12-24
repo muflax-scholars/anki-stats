bf41c5d7f4576a9492c3216004bc52da6122d105
muflax <mail@muflax.com>
2013-12-24 23:59:22 +0000

    calculate probability of remembering a card

--- anki_stats.rb
+++ anki_stats.rb
@@ -6,19 +6,17 @@
 require "muflax"
 require "sqlite3"
 
-RetentionRates = vivaHash 0.90
+AnswersByEase = vivaHash []
+RetentionRate = vivaHash 0.90
 AnkiFile = "~/anki/muflax/collection.anki2"
-
-def decay_prob days_since_last_review, last_interval, retention_rate
-  decay_rate = Math.log(retention_rate) / last_interval
-
-  prob = Math.exp(decay_rate * days_since_last_review)
-
-  prob
-end
 
 puts "opening #{AnkiFile}..."
 anki_db = SQLite3::Database.new(File.expand_path(AnkiFile))
+
+DeckCreation = anki_db.get_first_row("select crt from col").first
+StartDate    = Date.strptime(DeckCreation.to_s, "%s")
+Today        = Date.today
+puts "deck started on #{StartDate}"
 
 # due calculation:
 #
@@ -37,27 +35,79 @@ anki_db = SQLite3::Database.new(File.expand_path(AnkiFile))
 stats = anki_db.execute("select id, cid, ease, factor, ivl, type from revlog")
 puts "#{stats.size} stats loaded."
 
-stats.each do |id, cardId, ease, factor, ivl, type|
+stats.each do |id, cardId, answer, factor, interval, type|
+  # use the factor (within 10%) as the ease
+  ease = factor / 100
+
+  # only consider proper reviews
+  next unless type == 1 and interval > 0
+
+  # 1 -> wrong, 2..4 -> correct
+  AnswersByEase[ease] << (answer == 1 ? 0 : 1)
 end
 
-cards = anki_db.execute("select id, type, queue, due, ivl, factor from cards")
+# use the average of the retention rates, but cap at 99%
+AnswersByEase.each do |ease, answers|
+  RetentionRate[ease] = [answers.average, 0.99].min
+end
+
+Card = Struct.new :id, :type, :queue, :due, :interval, :factor do
+  def ease
+    factor / 100
+  end
+
+  def retention_rate
+    RetentionRate[ease]
+  end
+
+  def due_date
+    StartDate + due
+  end
+
+  def review_date
+    due_date - interval
+  end
+
+  def due?
+    due_date <= Today
+  end
+
+  def remember_prob day=0
+    decay_rate = Math.log(retention_rate) / interval
+
+    prob = Math.exp(decay_rate * ((Today - review_date) + day))
+
+    prob
+  end
+
+end
+cards = anki_db.execute("select id, type, queue, due, ivl, factor from cards").map{|c| Card.new(*c)}
 puts "#{cards.size} cards loaded."
 
 # cards without a history yet
 new_cards      = 0
 learning_cards = 0
 
-cards.each do |id, type, queue, due, ivl, factor|
-  case queue
+cards.each do |card|
+  case card.queue
   when 0 # new
     new_cards += 1
   when 1 # learning
     learning_cards += 1
   when 2, -2, -3 # review queue or buried
+    # ap retention_rate
+
+    ap(id: card.id,
+       r: card.review_date,
+       i: card.interval,
+       due: card.due_date,
+       p: card.remember_prob,
+       pt: card.remember_prob(1),
+       pw: card.remember_prob(7),
+       pm: card.remember_prob(30),
+      ) if card.due?
   when -1 # suspended
     # don't care
-  else
-    # shouldn't happen
   end
 end
 
