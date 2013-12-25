97bd77f6c2e23e5e017de5a5ee641348c9c88132
muflax <mail@muflax.com>
2013-12-25 00:59:51 +0000

    translate probability to time wasted

--- anki_stats.rb
+++ anki_stats.rb
@@ -32,12 +32,16 @@ puts "deck started on #{StartDate}"
 # - rev queue: integer day
 # - lrn queue: integer timestamp
 
-stats = anki_db.execute("select id, cid, ease, factor, ivl, type from revlog")
+TimeForCard = vivaHash 0
+
+stats = anki_db.execute("select id, cid, ease, factor, ivl, type, time from revlog")
 puts "#{stats.size} stats loaded."
 
-stats.each do |id, cardId, answer, factor, interval, type|
+stats.each do |id, cardId, answer, factor, interval, type, time|
   # use the factor (within 10%) as the ease
   ease = factor / 100
+
+  TimeForCard[cardId] += time / 1000.0
 
   # only consider proper reviews
   next unless type == 1 and interval > 0
@@ -51,7 +55,7 @@ AnswersByEase.each do |ease, answers|
   RetentionRate[ease] = [answers.average, 0.99].min
 end
 
-Card = Struct.new :id, :type, :queue, :due, :interval, :factor do
+Card = Struct.new :id, :type, :queue, :due, :interval, :factor, :time do
   def ease
     factor / 100
   end
@@ -68,8 +72,8 @@ Card = Struct.new :id, :type, :queue, :due, :interval, :factor do
     due_date - interval
   end
 
-  def due?
-    due_date <= Today
+  def due? day=0
+    due_date <= Today+day
   end
 
   def remember_prob day=0
@@ -80,6 +84,9 @@ Card = Struct.new :id, :type, :queue, :due, :interval, :factor do
     prob
   end
 
+  def time
+    TimeForCard[id]
+  end
 end
 cards = anki_db.execute("select id, type, queue, due, ivl, factor from cards").map{|c| Card.new(*c)}
 puts "#{cards.size} cards loaded."
@@ -87,6 +94,8 @@ puts "#{cards.size} cards loaded."
 # cards without a history yet
 new_cards      = 0
 learning_cards = 0
+
+work_wasted = vivaHash 0
 
 cards.each do |card|
   case card.queue
@@ -97,19 +106,33 @@ cards.each do |card|
   when 2, -2, -3 # review queue or buried
     # ap retention_rate
 
-    ap(id: card.id,
-       r: card.review_date,
-       i: card.interval,
-       due: card.due_date,
-       p: card.remember_prob,
-       pt: card.remember_prob(1),
-       pw: card.remember_prob(7),
-       pm: card.remember_prob(30),
-      ) if card.due?
+
+    # ap(id: card.id,
+    #    r: card.review_date,
+    #    i: card.interval,
+    #    due: card.due_date,
+    #    p: card.remember_prob,
+    #    pt: card.remember_prob(1),
+    #    pw: card.remember_prob(7),
+    #    pm: card.remember_prob(30),
+    #   ) if card.due?
+
+    if card.due?
+      [1, 7, 30, 365].each do |i|
+        work_wasted[i] += card.time * (card.remember_prob - card.remember_prob(i))
+      end
+    end
   when -1 # suspended
     # don't care
   end
 end
 
+puts
 puts "#{new_cards} unreviewed new cards."
 puts "#{learning_cards} cards in learning queue (counting as unlearned)."
+
+puts
+puts "%0.2f minutes of work wasted if you don't study today."       % (work_wasted[1]   / 60.0)
+puts "%0.2f minutes of work wasted if you don't study for a week."  % (work_wasted[7]   / 60.0)
+puts "%0.2f minutes of work wasted if you don't study for a month." % (work_wasted[30]  / 60.0)
+puts "%0.2f minutes of work wasted if you don't study for a year."  % (work_wasted[365] / 60.0)
