a6fd00565624fac7034dd76c5400b8743b582b8b
muflax <mail@muflax.com>
2014-01-04 04:32:28 +0000

    filter by deck

--- anki_stats.rb
+++ anki_stats.rb
@@ -5,6 +5,7 @@
 
 require "muflax"
 require "sqlite3"
+require "json"
 
 opts = Trollop::options do
   opt :database, "which database to use", :type => :string, :default => "~/anki/muflax/collection.anki2"
@@ -15,6 +16,7 @@ AnkiFile = File.expand_path(opts[:database])
 puts "opening #{AnkiFile}..."
 anki_db = SQLite3::Database.new(AnkiFile)
 
+# times needed for due calculations
 DeckCreation = anki_db.get_first_row("select crt from col").first
 StartDate    = Date.strptime(DeckCreation.to_s, "%s")
 Today        = Date.today
@@ -59,9 +61,13 @@ AnswersByEase.each do |ease, answers|
   RetentionRate[ease] = [answers.average, 0.95].min
 end
 
-Card = Struct.new :id, :type, :queue, :due, :interval, :factor, :time do
+Card = Struct.new :id, :deck_id, :type, :queue, :due, :interval, :factor, :time do
   def ease
     factor / 100
+  end
+
+  def deck
+    Decks[deck_id.to_s]["name"]
   end
 
   def retention_rate
@@ -97,91 +103,85 @@ Card = Struct.new :id, :type, :queue, :due, :interval, :factor, :time do
     Math.log(interval, ease / 10.0).round
   end
 end
-cards = anki_db.execute("select id, type, queue, due, ivl, factor from cards").map{|c| Card.new(*c)}
-puts "#{cards.size} cards loaded."
+all_cards = anki_db.execute("select id, did, type, queue, due, ivl, factor from cards").map{|c| Card.new(*c)}
+puts "#{all_cards.size} cards loaded."
 
-# stats
-new_cards      = 0
-learning_cards = 0
-review_cards   = vivaHash 0
-time_invested  = 0
-time_wasted    = vivaHash 0
-extra_reviews  = vivaHash 0
+# read decks
+Decks = JSON.load(anki_db.get_first_row("select decks from col").first)
 
-cards.each do |card|
-  time_invested += card.time
+cards_by_deck = all_cards.group_by(&:deck)
+puts "#{cards_by_deck.size} decks in use."
+puts
 
-  case card.queue
-  when 0 # new
-    new_cards += 1
-  when 1 # learning
-    learning_cards += 1
-  when 2, -2, -3 # review queue or buried
-    [0, 1, 7, 30, 365, 365*10].each do |i|
-      review_cards[i] += 1 if card.due? Today + i
+cards_by_deck.sort.each do |deck, cards|
+  # stats
+  new_cards      = 0
+  learning_cards = 0
+  review_cards   = vivaHash 0
+  time_invested  = 0
+  time_wasted    = vivaHash 0
+  extra_reviews  = vivaHash 0
 
-      if card.due? Today + i
-        prob_diff = card.remember_prob(card.due_date) - card.remember_prob(Today + i)
+  puts "#{deck}:"
+  cards.each do |card|
 
-        time_wasted[i]     += card.time  * prob_diff
-        extra_reviews[i]   += card.stage * prob_diff
+    time_invested += card.time
+
+    case card.queue
+    when 0 # new
+      new_cards += 1
+    when 1 # learning
+      learning_cards += 1
+    when 2, -2, -3 # review queue or buried
+      [0, 1, 7, 30, 365, 365*10].each do |i|
+        review_cards[i] += 1 if card.due? Today + i
+
+        if card.due? Today + i
+          prob_diff = card.remember_prob(card.due_date) - card.remember_prob(Today + i)
+
+          time_wasted[i]     += card.time  * prob_diff
+          extra_reviews[i]   += card.stage * prob_diff
+        end
       end
+    when -1 # suspended
+      # don't care
     end
-  when -1 # suspended
-    # don't care
   end
+
+  # show statistics
+  puts "#{new_cards} unreviewed new cards."
+  puts "#{learning_cards} cards in learning queue (counting as unlearned)."
+  puts "#{review_cards[0]} cards to review."
+  puts "#{review_cards[0] + learning_cards + new_cards} cards total to do."
+
+  puts
+  puts "%.1f min already invested." % time_invested
+
+  puts
+  puts " %6.1f           min already wasted."                        %  time_wasted[0]
+  puts "%7.1f (%+7.1f) min wasted if you don't study today."         % [time_wasted[1],
+    time_wasted[1]    - time_wasted[0]]
+  puts "%7.1f (%+7.1f) min wasted if you don't study for a week."    % [time_wasted[7],
+    time_wasted[7]    - time_wasted[0]]
+  puts "%7.1f (%+7.1f) min wasted if you don't study for a month."   % [time_wasted[30],
+    time_wasted[30]   - time_wasted[0]]
+  puts "%7.1f (%+7.1f) min wasted if you don't study for a year."    % [time_wasted[365],
+    time_wasted[365]  - time_wasted[0]]
+  puts "%7.1f (%+7.1f) min wasted if you don't study for ten years." % [time_wasted[3650],
+    time_wasted[3650] - time_wasted[0]]
+
+  puts
+  puts " %6.1f           extra reviews already added."                   %  extra_reviews[0]
+  puts "%+7.1f (%+7.1f) extra reviews if you don't study today."         % [extra_reviews[1],
+    extra_reviews[1]    - extra_reviews[0]]
+  puts "%+7.1f (%+7.1f) extra reviews if you don't study for a week."    % [extra_reviews[7],
+    extra_reviews[7]    - extra_reviews[0]]
+  puts "%+7.1f (%+7.1f) extra reviews if you don't study for a month."   % [extra_reviews[30],
+    extra_reviews[30]   - extra_reviews[0]]
+  puts "%+7.1f (%+7.1f) extra reviews if you don't study for a year."    % [extra_reviews[365],
+    extra_reviews[365]  - extra_reviews[0]]
+  puts "%+7.1f (%+7.1f) extra reviews if you don't study for ten years." % [extra_reviews[3650],
+    extra_reviews[3650] - extra_reviews[0]]
 
   puts
 end
-
-# show statistics
-puts "#{new_cards} unreviewed new cards."
-puts "#{learning_cards} cards in learning queue (counting as unlearned)."
-puts "#{review_cards[0]} cards to review."
-puts "#{review_cards[0] + learning_cards + new_cards} cards total to do."
-
-puts
-puts "%.1f min already invested." % time_invested
-
-puts
-puts " %6.1f           min already wasted."                        %  time_wasted[0]
-puts "%7.1f (%+7.1f) min wasted if you don't study today."         % [time_wasted[1],
-                                                                      time_wasted[1]    - time_wasted[0]]
-puts "%7.1f (%+7.1f) min wasted if you don't study for a week."    % [time_wasted[7],
-                                                                      time_wasted[7]    - time_wasted[0]]
-puts "%7.1f (%+7.1f) min wasted if you don't study for a month."   % [time_wasted[30],
-                                                                      time_wasted[30]   - time_wasted[0]]
-puts "%7.1f (%+7.1f) min wasted if you don't study for a year."    % [time_wasted[365],
-                                                                      time_wasted[365]  - time_wasted[0]]
-puts "%7.1f (%+7.1f) min wasted if you don't study for ten years." % [time_wasted[3650],
-                                                                      time_wasted[3650] - time_wasted[0]]
-
-time_wasted.each do |i, _|
-  next if i == 0
-  time_wasted[i] -= time_wasted[0]
-end
-
-puts
-puts "%7.1f min (%7.1f sec) cheaper than tomorrow."     % [time_wasted[1] / review_cards[1],
-                                                           time_wasted[1]    * 60 / review_cards[1]]
-puts "%7.1f min (%7.1f sec) cheaper than in a week."    % [time_wasted[7] / review_cards[7],
-                                                           time_wasted[7]    * 60 / review_cards[7]]
-puts "%7.1f min (%7.1f sec) cheaper than in a month."   % [time_wasted[30] / review_cards[30],
-                                                           time_wasted[30]   * 60 / review_cards[30]]
-puts "%7.1f min (%7.1f sec) cheaper than in a year."    % [time_wasted[365] / review_cards[365],
-                                                           time_wasted[365]  * 60 / review_cards[365]]
-puts "%7.1f min (%7.1f sec) cheaper than in ten years." % [time_wasted[3650] / review_cards[3650],
-                                                           time_wasted[3650] * 60 / review_cards[3650]]
-
-puts
-puts " %6.1f           extra reviews already added."                   %  extra_reviews[0]
-puts "%+7.1f (%+7.1f) extra reviews if you don't study today."         % [extra_reviews[1],
-                                                                          extra_reviews[1]    - extra_reviews[0]]
-puts "%+7.1f (%+7.1f) extra reviews if you don't study for a week."    % [extra_reviews[7],
-                                                                          extra_reviews[7]    - extra_reviews[0]]
-puts "%+7.1f (%+7.1f) extra reviews if you don't study for a month."   % [extra_reviews[30],
-                                                                          extra_reviews[30]   - extra_reviews[0]]
-puts "%+7.1f (%+7.1f) extra reviews if you don't study for a year."    % [extra_reviews[365],
-                                                                          extra_reviews[365]  - extra_reviews[0]]
-puts "%+7.1f (%+7.1f) extra reviews if you don't study for ten years." % [extra_reviews[3650],
-                                                                          extra_reviews[3650] - extra_reviews[0]]
